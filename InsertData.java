1a44be6343e7e16c3da2c8e0dc01f64f38b35144
Divang <divang.s@gmail.com>
2019-07-27 17:15:30 +0530

    Update InsertData.java

--- InsertData.java
+++ InsertData.java
@@ -1,6 +1,7 @@
 /**
 To compile this code
 javac -cp .:/usr/lib/hadoop/*:/usr/lib/hadoop-mapreduce/*:/usr/lib/hbase/* InsertData.java
+java -cp .:/usr/lib/hadoop/*:/usr/lib/hadoop-mapreduce/*:/usr/lib/hbase/*::/usr/lib/hbase/lib/* InsertData
 */
 
 import java.io.IOException;
